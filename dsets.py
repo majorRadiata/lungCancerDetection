b6326ae6179f84da64f51be527496710f82bd561
majorMokoto <nostromo426@protonmail.ch>
2021-09-16 18:37:51 +0300

    updating

--- dsets.py
+++ dsets.py
@@ -4,6 +4,11 @@ import glob
 import os
 import SimpleITK as sitk
 import numpy as np
+import copy
+import torch
+import torch.cuda
+from torch.utils.data import Dataset
+from util import XyzTuple, xyz2irc
 from collections import namedtuple
 
 # hold information for each nodule in named tuple
@@ -13,11 +18,12 @@ CandidateInfoTuple = namedtuple(
 )
 
 
-@functools.cache(1)
+@functools.lru_cache(1)
 def get_candidate_info_list(require_on_disk_bool=True):
     # getting information from two .csv files by series_uid
     mhd_list = glob.glob('E:/upload/LUNA/subset*/*.mhd')
     present_on_disk_set = {os.path.split(p)[-1][:-4] for p in mhd_list}
+
     diameter_dict = {}
     with open('E:/upload/LUNA/annotations.csv', "r") as f:
         for row in list(csv.reader(f))[1:]:
@@ -59,3 +65,86 @@ class Ct:
 
         ct_mhd = sitk.ReadImage(mhd_path)
         ct_a = np.array(sitk.GetArrayFromImage(ct_mhd), dtype=np.float32)
+        ct_a.clip(-1000, 1000, ct_a)
+        self.series_uid = series_uid
+        self.hu_a = ct_a
+        self.origin_xyz = XyzTuple(*ct_mhd.GetOrigin())
+        self.vx_size_xyz = XyzTuple(*ct_mhd.GetSpacing())
+        self.direction_a = np.array(ct_mhd.GetDirection()).reshape(3, 3)
+
+    def get_raw_candidate(self, center_xyz, width_irc):
+        center_irc = xyz2irc(
+            center_xyz,
+            self.origin_xyz,
+            self.vx_size_xyz,
+            self.direction_a
+        )
+        slice_list = []
+        for axis, center_val in enumerate(center_irc):
+            start_ndx = int(round(center_val - width_irc[axis]/2))
+            end_ndx = int(start_ndx + width_irc[axis])
+            slice_list.append(slice(start_ndx, end_ndx))
+            assert 0 <= center_val < self.hu_a.shape[axis], repr(([self.series_uid, center_xyz,
+                                                                   self.origin_xyz, self.vx_size_xyz, center_irc, axis]))
+            if start_ndx < 0:
+                start_ndx = 0
+                end_ndx = int(width_irc[axis])
+            if end_ndx > self.hu_a.shape[axis]:
+                end_ndx = self.hu_a.shape[axis]
+                start_ndx = int(self.hu_a.shape[axis] - width_irc[axis])
+
+            slice_list.append(slice(start_ndx, end_ndx))
+        ct_chunk = self.hu_a[tuple(slice_list)]
+
+        return ct_chunk, center_irc
+
+
+@functools.lru_cache(1, typed=True)
+def get_ct(series_uid):
+    return Ct(series_uid)
+
+
+def get_ct_raw_candidate(series_uid, center_xyz, width_irc):
+    ct = get_ct(series_uid)
+    ct_chunk, center_irc = ct.get_raw_candidate(center_xyz, width_irc)
+    return ct_chunk, center_irc
+
+class LunaDataset(Dataset):
+    def __init__(self, val_stride=0, is_valset_bool = None, series_uid=None):
+        self.candidate_info_list = copy.copy(get_candidate_info_list())
+
+        if series_uid:
+            self.candidate_info_list = [x for x in self.candidate_info_list if x.series_uid == series_uid]
+
+        if is_valset_bool:
+            assert val_stride > 0, val_stride
+            self.candidate_info_list = self.candidate_info_list[::val_stride]
+            assert self.candidate_info_list
+        elif val_stride > 0:
+            del self.candidate_info_list[::val_stride]
+            assert self.candidate_info_list
+
+    def __len__(self):
+        return len(self.candidate_info_list)
+
+    def __getitem__(self, ndx):
+        candidate_info_tup = self.candidate_info_list[ndx]
+        width_irc = (32, 48, 48)
+
+        candidate_a, center_irc = get_ct_raw_candidate(
+            candidate_info_tup.series_uid,
+            candidate_info_tup.center_xyz,
+            width_irc
+        )
+
+        candidate_t = torch.from_numpy(candidate_a)
+        candidate_t = candidate_t.to(torch.float32)
+        candidate_t = candidate_t.unsqueeze(0)
+
+        pos_t = torch.tensor([
+            not candidate_info_tup.is_nodule_bool,
+            candidate_info_tup.is_nodule_bool
+        ], dtype=torch.long)
+
+        return(candidate_t, pos_t,
+               candidate_info_tup.series_uid, torch.tensor(center_irc))
